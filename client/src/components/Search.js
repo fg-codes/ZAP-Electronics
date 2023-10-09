319b028cbb46a76a0b908a243033c5a261e03e55
Frederick Gitto <frederick_gitto@hotmail.com>
2023-10-09 17:49:00 -0400

    dotenv process endpoint change

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -23,7 +23,7 @@ export const Search = () => {
   }
 
   useEffect(() => {
-    fetch(`/search?q=${query}`)
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/search?q=${query}`)
       .then((res) => res.json())
       .then((data) => setSearchResults(data.data))
       .catch((error) => console.log(error))
