aa1326dbf144497908b6f1ccfdb98ba67c10451f
Frederick Gitto <frederick_gitto@hotmail.com>
2023-10-09 20:15:37 -0400

    add cors and allow origin

--- server/server.js
+++ server/server.js
@@ -254,13 +254,11 @@ express()
   .use(express.json())
   .use(express.urlencoded({ extended: false }))
   .use(cors({
-    origin: FE_ORIGIN_BASE_URL,
-    credentials: true,
+    origin: FE_ORIGIN_BASE_URL
   }))
   .use(function (req, res, next) {
-    res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Origin', FE_ORIGIN_BASE_URL);
-    res.header('Access-Control-Allow-Methods', 'GET,POST');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header(
       'Access-Control-Allow-Headers',
       'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
